8bdb9d29bdc89fd61e6a745fc0af8174c59e1c3a
jakebeaver <1st.cubix@gmail.com>
2020-12-07 12:55:27 +0100

    floating appbar in sessions view

--- lib/screens/sessionsScreen.dart
+++ lib/screens/sessionsScreen.dart
@@ -16,27 +16,44 @@ class SessionsScreen extends StatelessWidget {
     return Scaffold(
       key: scaffoldKey,
       backgroundColor: Color(0xff001538),
-      appBar: appbar,
+      // appBar: appbar,
       floatingActionButton: prov.disconnectedIcon(scaffoldKey),
-      body: RefreshIndicator(
-        onRefresh: () => prov.refresh(context),
-        child: prov.isLoading
-            ? Center(child: CircularProgressIndicator())
-            : ListView(
-                shrinkWrap: true,
-                children: [
-                  // ...prov.playlistRanks.map((x) => RankWidget(x)).toList(),
-                  // if (prov.sessions.length > 0)
-                  // SizedBox(height: 20),
-                  // if (prov.sessions.length > 0)
-                  // SessionWidget(prov.sessions[0], latest: true),
-
-                  ...prov.sessions
-                      .map((session) => SessionWidget(session))
-                      .toList(),
-                ],
-              ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text("Sessions"),
+            backgroundColor: Color(0xff041d59),
+            floating: true,
+          ),
+          new SliverList(
+            delegate: new SliverChildListDelegate(
+              prov.sessions.map((session) => SessionWidget(session)).toList(),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+
+  RefreshIndicator getSessionsView(TrackerData prov, BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: () => prov.refresh(context),
+      child: prov.isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView(
+              shrinkWrap: true,
+              children: [
+                // ...prov.playlistRanks.map((x) => RankWidget(x)).toList(),
+                // if (prov.sessions.length > 0)
+                // SizedBox(height: 20),
+                // if (prov.sessions.length > 0)
+                // SessionWidget(prov.sessions[0], latest: true),
+
+                ...prov.sessions
+                    .map((session) => SessionWidget(session))
+                    .toList(),
+              ],
+            ),
     );
   }
 }
